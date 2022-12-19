bb60850c35f870ccb1f5d1c8a948bebc3a6cfb94
jonseb974 <5151@holbertonstudents.com>
2022-12-19 10:43:19 +0100

    update to files issues

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
 """
-Flask app to generate
-complete html page containing location/amenity
-dropdown menus and rental listings
+Flask app integrates in AirBnB HTML template.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from models import storage
 import uuid
-app = Flask('web_dynamic')
+
+# Set up for flask
+app = Flask(__name__)
 app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+
+# flask rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+	"""
+	after each request, the method calls .close(), .remove() on
+	current SQLAlchemy Session
+	"""
+	storage.close()
 
 
 @app.route('/0-hbnb/')
-def display_hbnb():
-    """Generate page with popdown menu of states/cities"""
-    states = storage.all('State')
-    amenities = storage.all('Amenity')
-    places = storage.all('Place')
-    cache_id = uuid.uuid4()
-    return render_template('1-hbnb.html',
-                           states=states,
-                           amenities=amenities,
-                           places=places,
-                           cache_id=cache_id)
-
-
-@app.teardown_appcontext
-def teardown_db(*args, **kwargs):
-    """Close database or file storage"""
-    storage.close()
+def hbnh_filters(the_id=None):
+	"""
+	Custom template with states, cities, amenities.
+	"""
+	state_objs = storage.all('State').values()
+	states = dict([state.name, state] for state in state_objs)
+	amens = storage.all('Amenity').values()
+	places = storage.all('Places').vales()
+	users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+		          for user in storage.all('User').value())
+	cache_id = (str(uuid.uuid4()))
+	return render_template('1-hbnb.html',
+		                    states=states,
+		                    amens=amens,
+		                    places=places,
+		                    users=users,
+		                    cache_id=cache_id)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+	"""
+	The main Flask app
+	"""
+	app.run(host=host, port=port)
--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 """
-Flask App that integrates with AirBnB static HTML Template
+Flask App that integrates with AirBnB static HTML Template.
 """
 import uuid
 from flask import Flask, render_template, url_for
@@ -26,7 +27,7 @@ def teardown_db(exception):
 @app.route('/1-hbnb')
 def hbnb_filters(the_id=None):
     """
-    handles request to custom template with states, cities & amentities
+    handles request to custom template with states, cities & amenities
     """
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
